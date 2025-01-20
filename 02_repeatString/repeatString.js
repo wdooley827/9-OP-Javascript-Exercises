f27ff61e17d56e2a89677173ba5a8386668bd09e
Will <wdooley311@gmail.com>
2025-01-20 10:14:35 -0500

    Tested function under various conditions, inputing a string X number of times.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+if (times < 0) return "Please input a value greater than 0"
+let string = "";
+for (let i =0; i < times; i++) {
+    string+=word; 
+}
+    return string;
 
 };
 
