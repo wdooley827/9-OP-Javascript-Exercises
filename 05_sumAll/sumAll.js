7994ed3be0bc5e99720bfc8e52fdef2b5de51c63
Will <wdooley311@gmail.com>
2025-01-22 11:39:19 -0500

    Utilized array method isInteger, booleans, and for loop to create a sum function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
 const sumAll = function() {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) [min, max] = [max, min];
 
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
